2bff62fbf0fdef9d5fe2c211862cff0247efa588
Emre Yılmaz <mail@emreyilmaz.me>
2013-05-19 16:46:01 +0300

    added setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='macster',
+    version='0.1',
+    packages=['macster', ],
+    url='http://github.com/emre/macster',
+    license='MIT',
+    author='Emre Yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='DS_Store generator for your happiness.',
+
+)
